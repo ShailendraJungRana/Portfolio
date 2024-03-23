d1ec3d42c2832e3314b57a63a29d6d6047b1386d
Arun jR <arun@Aruns-MacBook-Pro.local>
2024-03-23 23:44:14 +0545

    start from Navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        {/* // <FontAwesomeIcon icon="fa-solid fa-bars" /> */}
+        <h1 className="text-4xl font-bold font-signature " >ShailendraJR</h1>
+    </div>
+  )
+}
+
+export default Navbar
+
