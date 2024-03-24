234b400a46a5b563bff970b208e2a0b9feee53a1
Arun jR <arun@Aruns-MacBook-Pro.local>
2024-03-24 23:59:02 +0545

    Icon on

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const Navbar = () => {
   return (
     <div>
-        {/* // <FontAwesomeIcon icon="fa-solid fa-bars" /> */}
+         <FontAwesomeIcon icon="fa-solid fa-bars" />
         <h1 className="text-4xl font-bold font-signature " >ShailendraJR</h1>
     </div>
   )
