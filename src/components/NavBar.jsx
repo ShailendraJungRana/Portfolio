36a70bac3e621661367163b94fcc7cac4aaaf2ba
Arun jR <arun@Aruns-MacBook-Pro.local>
2024-03-25 23:43:01 +0545

    Responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, FaTimes } from '@fortawesome/free-solid-svg-icons'; // Import the specific icon
-
-
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'; // Import the specific icon
+import React, {useState} from 'react';
 
 const Navbar = () => {
+  const [nav,setNav]= useState(false);
+
   const links=[
     {
       id:1,
@@ -31,21 +32,34 @@ const Navbar = () => {
       <div>
         <h1 className="text-4xl font-bold font-signature ml-2 px-5 cursor-pointer" >ShailendraJR</h1>
       </div>
-      <ul className='flex'>
+      <ul className='hidden md:flex'>
 
       {links.map(({id,link}) =>(
-           <li key={link , id} className='px-5 cursor-pointer capitalize font-medium 
-           text-black-500 hover:scale-105 duration-200'>{link}</li>
+           <li key={id} className='px-5 cursor-pointer capitalize font-medium 
+           text-black-500 hover:scale-105 duration-200'>{link}</li> //key={link,id}
       ))}
-
-       
-       
       </ul>
+      <div onClick={()=>setNav(!nav)} className='cursor-pointer pr-6 z-10 text-grey-500 md:hidden'>
+        {nav ?  <FontAwesomeIcon icon={faTimes} style ={{fontSize: "24px"}}/> : <FontAwesomeIcon icon={faBars} style ={{fontSize: "24px"}}/>}
+      </div>
+
+        {nav && (
+          
+          
+          <ul className='flex flex-col justify-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-white to-gray-800 text-black'>
+      {links.map(({id,link}) =>(
+        <li key={id} className='px-4 cursor-pointer capitalize py-6 text-3xl'>{link}</li> //key={link,id}
+        ))} 
+
+      </ul>
+        )}
     </div>
   )
 }
-// <FontAwesomeIcon icon={faBars} />
 
+
+
+// {/* <FontAwesomeIcon icon={faBars} style ={{fontSize: "24px"}}/> */}
 
 export default Navbar
 
