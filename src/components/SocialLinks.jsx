951de2b5ef72499500e98f6b6daa66a05259ebd3
Arun jR <arun@Aruns-MacBook-Pro.local>
2024-03-29 10:06:18 +0545

    SideIcon Proper Functioning

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { FaGithub , FaLinkedin } from "react-icons/fa";
 // import { SiGmail } from "react-icons/si";
-
+import {HiOutlineMail} from 'react-icons/hi';
+import {BsFillPersonLinesFill} from 'react-icons/bs';
 
 function SocialLinks() {
     const links = [
@@ -28,26 +29,44 @@ function SocialLinks() {
         href:"https://github.com/ShailendraJungRana",
     },
     {
-        id:1,
+        id:3,
         child:(
             <>
-            LinkedIn <FaLinkedin  size={30}/> 
-
+            Mail<HiOutlineMail size={30}/>
             </>
         ),
-        href:"https://www.linkedin.com/in/shailendra-jung-rana-aa78781a1/",
+        href : "mailto:shailendera.jungrana@gmail.com",
         style:'rounderd-tr-md' 
+    },
+    {
+        id:4,
+        child:(
+            <>
+            Resume<BsFillPersonLinesFill size={30}/>
+            </>
+        ),
+        href : '/resume.pdf',
+        style:'rounderd-br-md',
+        download: true,
     },
 ];
 
         return (
 <div className='flex flex-col top-[35%] left-0 fixed'>
       <ul>
-        <li className=' flex justify-between items-center w-40 h-14 px-4 bg-white ml-[-100px] hover:ml-[-10px] hover:rounder-md duration-300 bg-white-500'>
-            <a href="https://www.linkedin.com/in/shailendra-jung-rana-aa78781a1/" className='flex justify-between items-center w-full text-black '>
-               
-            </a>
-        </li>
+        {links.map(({id, child, href,style,download})=>(
+            <li key={id}
+                className= {"flex justify-between items-center w-40 h-14 px-4 bg-white ml-[-100px] hover:ml-[-10px] hover:rounder-md duration-300 bg-white-500" + ""+ style}>
+             <a href={href} 
+                className='flex justify-between items-center w-full text-black h-14 '
+                download={download}
+                target='_blank'
+              >
+                 {child}
+             </a>
+             </li>
+        ))}
+      
       </ul>
     </div>
   )
